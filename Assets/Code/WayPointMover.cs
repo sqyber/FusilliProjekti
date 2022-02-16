ae3cec68e62f443ac5af0cacb1a3325a19ff8b06
Samu <sqyber@gmail.com>
2022-02-16 22:59:56 +0200

    Lähetin waypoint systeemi

---
+++ Assets/Code/WayPointMover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointMover : MonoBehaviour
+{
+    
+    // reference to the waypoint system
+    [SerializeField] private Waypoints waypoints;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Transform currentWaypoint;
+    
+    
+    void Start()
+    {
+        //set initial position to the first waypoint
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+        
+        //set the next waypoint target
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =
+            Vector2.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, currentWaypoint.position) < 0.1)
+        {
+            currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        }
+    }
+}
---
+++ Assets/Code/Waypoints.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+    [SerializeField] private float waypointSize = 1f;
+    private void OnDrawGizmos()
+    {
+        foreach (Transform t in transform)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(t.position, 1f);
+        }
+
+        Gizmos.color = Color.red;
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position);
+        }
+        //go to first child
+        Gizmos.DrawLine(transform.GetChild(transform.childCount -1).position, transform.GetChild(0).position);
+    }
+
+    
+    // doesn't work. only moves from 0 --> 1, not further.
+    public Transform GetNextWaypoint(Transform currentWaypoint)
+    {
+        if (currentWaypoint == null)
+        {
+            return transform.GetChild(0);
+        }
+
+        if (currentWaypoint.GetSiblingIndex() < transform.childCount - 1)
+        {
+            return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1);
+        }
+        else
+        {
+            return transform.GetChild(0);
+        }
+    }
+}
