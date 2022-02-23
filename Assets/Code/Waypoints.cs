c8ce891875e7f832e44aec5082e14e93cb6938d1
Lauri <lauri.kalliola97@outlook.com>
2022-02-23 10:24:15 +0200

    Fixed sizes and removed dead code

--- Assets/Code/Waypoints.cs
+++ Assets/Code/Waypoints.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class Waypoints : MonoBehaviour
 {
-
-    [SerializeField] private float waypointSize = 1f;
     private void OnDrawGizmos()
     {
         foreach (Transform t in transform)
