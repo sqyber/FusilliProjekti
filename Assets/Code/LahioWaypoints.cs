c01fd9b4f6071f2cd9e7a68d103a467ffe219ef3
Samu <sqyber@gmail.com>
2022-03-10 21:16:03 +0200

    Waypoints work fully
    Both roadwaypoint and lahiowaypoints work flawlessly

--- Assets/Code/LahioWaypoints.cs
+++ Assets/Code/LahioWaypoints.cs
@@ -8,27 +8,45 @@ public class LahioWaypoints : Waypoints
 {
     
     System.Random rnd = new System.Random();
+    bool delivered;
     
     public override Transform GetNextWaypoint(Transform currentWaypoint)
     {
-        // the cap of houses +1 --> 6 max = 5 houses
-       int housenumber = rnd.Next(1, 6);
-      
-        
-        if (currentWaypoint.GetSiblingIndex() == 0)
-        {
-            return transform.GetChild(housenumber);
-        }
-        else if(currentWaypoint == null)
-        {
-            //wait for button
+        // gives number between 1 and child count
+       int housenumber = rnd.Next(1, transform.childCount);
+       // marks for lahetti to move to next rail
+       
 
-            return transform.GetChild(0);
-        }
-        else
-        {
-            return null;
-        }
+
+       if (currentWaypoint == null)
+       {
+           //wait for button
+
+           return transform.GetChild(0);
+       }
+
+       if (currentWaypoint.GetSiblingIndex() == 0 && !delivered)
+       {
+           
+           //pick random house 
+           return transform.GetChild(housenumber);
+
+           
+       }
+       if (currentWaypoint.GetSiblingIndex() != 0)
+       {
+           //change bool to mark delivery done and prepare for next waypoint system
+           delivered = true;
+           return transform.GetChild(0);
+       }
+
+       if (delivered)
+       {
+           GetNextSystem();
+           delivered = false;
+       }
+       
+       return null;
     }
 }
 
