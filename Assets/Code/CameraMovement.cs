c517615e19ba7e99e889bcd961f0da548b9764d5
Lauri <lauri.kalliola97@outlook.com>
2022-03-11 10:32:36 +0200

    Android build + camera movement

---
+++ Assets/Code/CameraMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // This is used to store the information of the initial tap
+    Vector3 hit_position = Vector3.zero;
+    
+    // This is used to store the information of the current camera position
+    Vector3 current_position = Vector3.zero;
+    
+    // This is used to calculate the new camera position
+    Vector3 camera_position = Vector3.zero;
+    float z = 0.0f;
+
+    void Update(){
+        // Detection for the initial tap, for taking information
+        if(Input.GetMouseButtonDown(0)){
+            hit_position = Input.mousePosition;
+            camera_position = transform.position;
+
+        }
+        
+        // Detection for holding finger on the screen, if held go on to drag method
+        if(Input.GetMouseButton(0)){
+            current_position = Input.mousePosition;
+            LeftMouseDrag();        
+        }
+    }
+
+    // Drag method for holding finger down on screen
+    void LeftMouseDrag(){
+        current_position.z = hit_position.z = camera_position.y;
+
+        // Get direction of movement.
+        if (Camera.main != null)
+        {
+            Vector3 direction = Camera.main.ScreenToWorldPoint(current_position) - Camera.main.ScreenToWorldPoint(hit_position);
+
+            // Invert direction
+            direction = direction * -1;
+
+            Vector3 position = camera_position + direction;
+
+            transform.position = position;
+        }
+    }
+}
