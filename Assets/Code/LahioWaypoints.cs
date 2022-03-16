730e5264d60f7757b40407ac1600d956a4758578
Samu <sqyber@gmail.com>
2022-03-16 11:52:37 +0200

    Switch case template

--- Assets/Code/LahioWaypoints.cs
+++ Assets/Code/LahioWaypoints.cs
@@ -12,45 +12,76 @@ public class LahioWaypoints : Waypoints
     System.Random rnd = new System.Random();
     
     
+    
+
     public override Transform GetNextWaypoint(Transform currentWaypoint)
     {
         // gives number between 1 and child count
-       int housenumber = rnd.Next(1, transform.childCount);
-       // marks for lahetti to move to next rail
+        int housenumber = rnd.Next(1, transform.childCount);
+        // marks for lahetti to move to next rail
        
 
-       
 
-       if (currentWaypoint == null)
-       {
-           //Move to first waypoint
 
-           return transform.GetChild(0);
-       }
+        if (currentWaypoint == null)
+        {
+            //Move to first waypoint
 
-       if (currentWaypoint.GetSiblingIndex() == 0 && !delivered)
-       {
-           delivered = true;
+            return transform.GetChild(0);
+        }
 
-           //pick random house 
-           return transform.GetChild(housenumber);
-           
+        if (currentWaypoint.GetSiblingIndex() == 0 && !delivered)
+        {
+            delivered = true;
 
-           
-       }
-       else if (currentWaypoint.GetSiblingIndex() == 0 && delivered)
-       {
-           return null;
-       }
-       if (currentWaypoint.GetSiblingIndex() != 0)
-       {
-           //change bool to mark delivery done and prepare for next waypoint system
-           return transform.GetChild(0);
-       }
+            //pick random house 
+            return transform.GetChild(housenumber);
 
-       
-       
-       return null;
+
+
+        }
+        else if (currentWaypoint.GetSiblingIndex() == 0 && delivered)
+        {
+            return null;
+        }
+
+        if (currentWaypoint.GetSiblingIndex() != 0)
+        {
+            //change bool to mark delivery done and prepare for next waypoint system
+            return transform.GetChild(0);
+        }
+
+        return null;
+    }
+
+    public enum Status
+    {
+        Arrived,
+        Delivered,
+        Leaving
+    }
+
+    public Transform Delivery(Transform currentWaypoint, Status statuscheck){
+        int housenumber = rnd.Next(1, transform.childCount);
+
+        switch (statuscheck)
+        {
+            case Status.Arrived:
+                return transform.GetChild(housenumber);
+            
+
+            case Status.Delivered:
+                return transform.GetChild(0);
+            case Status.Leaving:
+                GetNextSystem();
+                break;
+            
+            default:
+                return null;
+        }
+
+        return null;
     }
 }
+
 
